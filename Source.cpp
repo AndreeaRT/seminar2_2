25e01420afef6344013b9f3f6dcc56f5e2ed1ef5
AndreeaRT <61688809+AndreeaRT@users.noreply.github.com>
2020-03-31 00:01:25 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+//I'm not sure I understood this problem right
+long double Struktur(bool ok, char s, int a, long b, double c, long double d)
+{
+	if (a != 0 || b != 0 || c != 0 || d != 0)
+		bool ok = 0;
+	else
+		bool ok = 1;
+	return
+}
+
+long double main(bool ok, char s, int a, long b, double c, long double d)
+{
+	int field = 0;
+	int a = 50,
+		long b = 32,
+		double c = 450,
+		long double d = 45;
+	char s[] = "Fitter, happier";
+	//a
+	if (ok == 0)
+
+		return 0;
+
+	//b
+	else
+		field = a + b + c + d;
+		return field;
+
+	return 0;
+
+}
+
